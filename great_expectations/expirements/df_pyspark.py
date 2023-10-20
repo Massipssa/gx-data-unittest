6d7fcbfe1cc70806b2324cab0e18e846b8a71e41
Massipssa Massipssa <kerrache.massipssa@gmail.com>
2023-10-20 18:08:18 +0000

    Add test

--- great_expectations/expirements/df_pyspark.py
+++ great_expectations/expirements/df_pyspark.py
@@ -3,7 +3,9 @@ import great_expectations as gx
 from ruamel import yaml
 from great_expectations.core.batch import RuntimeBatchRequest
 
+
 if __name__ == '__main__':
+
     base_path = "../data"
     datasource_name = "my_filesystem_datasource"
     # data_asset: is collection of records within datasource
@@ -34,6 +36,7 @@ if __name__ == '__main__':
 
     # add datasource to the context
     context.add_datasource(**datasource_config)
+    
     """
     Batch: is unique subset of Data Asset
     Batch Request
@@ -53,11 +56,33 @@ if __name__ == '__main__':
 
     # Expectations suite
     suite_name = "test_suite"
-    context.add_or_update_expectation_suite(expectation_suite_name=suite_name)
+    suite = context.add_or_update_expectation_suite(expectation_suite_name=suite_name)
+
+    # Create expectation config
+    expectation_config = gx.core.ExpectationConfiguration(
+        expectation_type="expect_column_values_to_not_be_null",
+        kwargs={
+            "column": "dockcount"
+        },
+        meta={
+            "notes": {
+                "format": "markdown",
+                "content": "Dockcount must be not null"
+            }
+        }
+    )
+
+    # add expectation
+    suite.add_expectation(expectation_config)
+
+    # Create the validator
     validator = context.get_validator(
         batch_request=batch_request,
         expectation_suite_name=suite_name
     )
+
+    # add expectation
+    validator.expectation_suite.add_expectation(expectation_configuration=config)
 
     print(validator.head())
     print(validator.active_batch.data.dataframe.count())
